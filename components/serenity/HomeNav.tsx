1e751ff25904e6be2a9530bdf0b14fb4146dc701
Ayushmaan0101 <ayushmaansingh304@gmail.com>
2024-09-15 19:54:53 +0530

    add: templates

--- components/serenity/HomeNav.tsx
+++ components/serenity/HomeNav.tsx
@@ -9,8 +9,6 @@ import Search from './Search';
 import HamburgurNavbar from './HamburgerNavbar';
 import CommandSearch from './CommandSearch';
 
-
-
 const HomeNav: React.FC = () => {
   return (
     <div className='navbar-container fixed top-0 left-0 right-0 bg-black border border-zinc-900 backdrop-blur-lg h-auto py-4 px-3 text-sm z-30 sm:mt-5 sm:mx-8 sm:rounded-full sm:py-3 flex justify-between items-center'>
@@ -19,13 +17,18 @@ const HomeNav: React.FC = () => {
         <Link href='/'>
           <p className='hidden sm:flex text-lg text-white ml-1'>Serenity UI</p>
         </Link>
-        <div className='hidden sm:flex items-center ml-10 space-x-8 text-[#ABAFB4]'>
-          <Link href='/docs/introduction' passHref>
-            <span className='hover:text-zinc-300 cursor-pointer'>Docs</span>
-          </Link>
-          <Link href='/components' prefetch passHref>
-            <span className='hover:text-zinc-300 cursor-pointer'>Components</span>
-          </Link>
+        <div className=''>
+          <div className='items-center flex space-x-6 text-[#ABAFB4]'>
+            <Link href='/components' passHref>
+              <span className='hover:text-zinc-300 cursor-pointer hidden sm:flex ml-10'>Components</span>
+            </Link>
+            <Link href='/templates' prefetch passHref>
+              <div className='flex items-center space-x-2'>
+                <span className='hover:text-zinc-300 cursor-pointer'>Templates</span>
+                <span className='text-xs  border border-green-400 text-green-400 rounded-full px-2 py-[2px]'>New</span>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
       <div className='flex items-center gap-1'>
