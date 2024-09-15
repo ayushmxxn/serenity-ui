a7ca0f9c92fae453bb0a4f5bd3d48c22be5ada9b
Ayushmaan0101 <ayushmaansingh304@gmail.com>
2024-09-15 19:53:30 +0530

    add: templates page

---
+++ app/templates/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/serenitypro/Navbar';
+import Section from '@/components/serenitypro/Section';
+import React from 'react'
+
+
+function TemplatesPage() {
+  return (
+    <div>
+      <Navbar/>
+      <Section/>
+    </div>
+  )
+}
+
+export default TemplatesPage;
