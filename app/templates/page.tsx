f1f3c2f8d620315d67221152ebe58f98d793faa1
Ayushmaan0101 <ayushmaansingh304@gmail.com>
2024-09-15 20:15:48 +0530

    add: template metadata

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -1,7 +1,11 @@
 import Navbar from '@/components/serenitypro/Navbar';
 import Section from '@/components/serenitypro/Section';
+import { Metadata } from 'next';
 import React from 'react'
 
+export const metadata: Metadata = {
+  title: "Templates",
+};
 
 function TemplatesPage() {
   return (
