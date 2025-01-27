f9e590f63291fb928416d017a2d179d48a2c8aaa
ayushmxxn <ayushmaansingh304@gmail.com>
2025-01-28 03:52:59 +0530

    fix: gumroad link

--- components/serenitypro/Section.tsx
+++ components/serenitypro/Section.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import PortfolioTemplateCard from './Card'
-
+import React from "react";
+import PortfolioTemplateCard from "./Card";
 
 function Section() {
   return (
     <div className="bg-black flex items-center justify-center mb-5 mt-20 gap-5 p-3 sm:p-5 flex-col md:flex-row">
       <PortfolioTemplateCard
-        previewUrl='https://preview-portfolio-template.netlify.app/'
-        buyUrl='https://ayushmxxn.gumroad.com/l/portfolio-template/4jco7wu'
-        imageUrl='https://i.ibb.co/fxZmkfL/portfoliotemplate.png'
-        hoverImageUrl='https://i.ibb.co/tMFnHqC/Screenshot-2024-09-15-182118.png'
+        previewUrl="https://preview-portfolio-template.netlify.app/"
+        buyUrl="https://ayushmxxn.gumroad.com/l/portfolio-template"
+        imageUrl="https://i.ibb.co/fxZmkfL/portfoliotemplate.png"
+        hoverImageUrl="https://i.ibb.co/tMFnHqC/Screenshot-2024-09-15-182118.png"
         title="Portfolio Template"
         description="Modern and professional template to showcase your best work."
         price={5}
@@ -17,11 +16,11 @@ function Section() {
           "Responsive",
           "SEO optimized",
           "Easily Customizable",
-          "Light and dark mode"
+          "Light and dark mode",
         ]}
       />
     </div>
-  )
+  );
 }
 
-export default Section
+export default Section;
